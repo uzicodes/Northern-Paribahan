35047ec4fc6d24d1da278c587b7d1b94cf3092ba
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-11-05 20:12:53 +0600

    chore(workspaces): enable npm workspaces and consolidate installs

---
+++ frontend/.next/static/chunks/app_layout_tsx_0a548d63._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/app_globals_71f961d1.css",
+  "static/chunks/node_modules_next_dist_3c980371._.js"
+],
+    source: "dynamic"
+});
