54f0840a0c998189180e60ce59611835feb06a17
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-11-05 20:17:13 +0600

    chore: untrack .next build artifacts

--- frontend/.next/static/chunks/app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_globals_71f961d1.css",
-  "static/chunks/node_modules_next_dist_53bfa806._.js",
-  "static/chunks/components_NavbarClient_tsx_a1a3c55e._.js"
-],
-    source: "dynamic"
-});
