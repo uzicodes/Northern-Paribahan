86eb37e861ae551fb4b65afe7a4baa26a6bdb7f1
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-12-23 00:26:02 +0600

    feat: added a profile page & a avatar in the navabar for direct navigatioin

--- frontend/components/NavbarClient.tsx
+++ frontend/components/NavbarClient.tsx
@@ -74,6 +74,14 @@ export default function NavbarClient() {
             Register
           </Link>
           <Link 
+            href="/profile"
+            className={isActive('/profile') ? 'text-[#FCA311] hover:text-[#FCA311]' : 'text-white hover:text-gray-300'}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+            </svg>
+          </Link>
+          <Link 
             href="/mail" 
             className={isActive('/mail') ? 'text-[#FCA311] hover:text-[#FCA311]' : 'text-white hover:text-gray-300'}
           >
